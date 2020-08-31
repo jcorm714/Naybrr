3f7b21bb2803a9d2736c6893cb7b125ec03414c5
Jamila Sisco <jsisco@email.neit.edu>
2020-08-31 19:49:00 -0400

    Lambda

---
+++ QT/Naybrr/db_controller.cpp
@@ -0,0 +1,6 @@
+#include "db_controller.h"
+
+db_controller::db_controller()
+{
+
+}
---
+++ QT/Naybrr/db_controller.h
@@ -0,0 +1,11 @@
+#ifndef DB_CONTROLLER_H
+#define DB_CONTROLLER_H
+
+
+class db_controller
+{
+public:
+    db_controller();
+};
+
+#endif // DB_CONTROLLER_H
